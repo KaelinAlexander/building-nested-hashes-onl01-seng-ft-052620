09ec8344d8a508f68946f7414033090986dcaaf9
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-01 16:35:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,12 +2,14 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
-        },
+        name: "Lord Montague",
+        age: "53"
       :matriarch => {
-        #your key/value pairs here
-        },
+        name: "Lady Montague",
+        age: "54"
       :hero => {
+        name: "Romeo",
+        age: "15"
         #your key/value pairs here
         },
       :hero_friends => []
